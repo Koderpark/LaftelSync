003d72c95ef9bc1212d5ac31bf49602271908bbd
박성훈 / Koder <koder0205@gmail.com>
2022-06-01 03:05:12 +0900

    update
    마참내!!!!
    대충 데이터 주고받기
    이제웹소켓배우러감 ㅅㄱ

--- js/background.js
+++ js/background.js
@@ -1,5 +1,5 @@
 // 브라우저 익스텐션이 라프텔 사이트 내에서만 활성화되도록 허용.
-chrome.runtime.onInstalled.addListener(function() {
+/*chrome.runtime.onInstalled.addListener(function() {
   chrome.action.disable();
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
@@ -11,25 +11,30 @@ chrome.runtime.onInstalled.addListener(function() {
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
-});
+});*/
 
 // IsWatchingVideo : 비디오태그가 있는 경우 true, 없으면 false를 반환.
 async function IsWatchingVideo(){
-  let [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-  let val   = await chrome.scripting.executeScript({
-    target: {tabId : tab.id},
-    function: () => {
-      return (document.getElementsByTagName('video').length != 0);
-    }
-  });
-  return val[0].result;
+  let ret = await chrome.tabs.query({ active: true, currentWindow: true });
+  if((ret[0].url).includes('laftel.net/player')){
+    return true;
+  }
+  else{
+    return false;
+  }
+  return ret[0].url;
 }
 
-chrome.runtime.onMessage.addListener( async (msg,sender,sendResponse) => {
-  if(msg.message == 'ChkWatching'){
-    let test = await IsWatchingVideo();
-    console.log(test);
-    sendResponse({message: test});
-  }
-  sendResponse({ message: 'asdfasdf' });
+chrome.runtime.onMessage.addListener( function (msg,sender,sendResponse){
+  //if(msg.message == 'ChkWatching'){
+  //  console.log(IsWatchingVideo());
+  //  sendResponse({message: IsWatchingVideo()});
+  //}
+  IsWatchingVideo().then((ret) => {
+    console.log(ret);
+    sendResponse({message: ret});
+  });
+  //sendResponse({message: "Hello World"});
+  return true;
 });
+
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.getElementById('makebtn').onclick = () => {
   chrome.runtime.sendMessage({
     message: 'ChkWatching'
-  }, (response) => {
-    alert(response.message);
+  }, function(ret){
+    console.log(ret);
   });
 }
