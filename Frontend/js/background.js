4d35b964d13c3d0ef6388f76fe868177a4b686f2
박성훈 / Koder <koder0205@gmail.com>
2022-06-02 02:09:48 +0900

    폴더구조 변경
    백엔드 / 프론트엔드 / 익스텐션
    셋으로 분리.
    백엔드엔 supervisor로 교체

--- Frontend/js/background.js
+++ Frontend/js/background.js
@@ -1,5 +1,5 @@
 // 브라우저 익스텐션이 라프텔 사이트 내에서만 활성화되도록 허용.
-/*chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function() {
   chrome.action.disable();
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
@@ -11,25 +11,29 @@
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
-});*/
+});
 
 // IsWatchingVideo : 비디오태그가 있는 경우 true, 없으면 false를 반환.
 async function IsWatchingVideo(){
-  let ret = await chrome.tabs.query({ active: true, currentWindow: true });
+  const ret = await chrome.tabs.query({ active: true, currentWindow: true });
   if((ret[0].url).includes('laftel.net/player')){
+
     return true;
   }
   else{
+    chrome.scripting.executeScript({
+      target: {tabId: ret[0].id},
+      func: () => { alert('애니메이션이 재생중일때만 파티를 만들수 있습니다'); }
+    });
     return false;
   }
-  return ret[0].url;
 }
 
 chrome.runtime.onMessage.addListener( function (msg,sender,sendResponse){
-  //if(msg.message == 'ChkWatching'){
-  //  console.log(IsWatchingVideo());
-  //  sendResponse({message: IsWatchingVideo()});
-  //}
+  if(msg.message == 'MakingParty'){
+    console.log(IsWatchingVideo());
+    sendResponse({message: IsWatchingVideo()});
+  }
   IsWatchingVideo().then((ret) => {
     console.log(ret);
     sendResponse({message: ret});
--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -1,6 +1,6 @@
 document.getElementById('makebtn').onclick = () => {
   chrome.runtime.sendMessage({
-    message: 'ChkWatching'
+    message: 'MakingParty'
   }, function(ret){
     console.log(ret);
   });
